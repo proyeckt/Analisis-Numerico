a907b83092f5a03569e7718dd2d6b01bc9cfcf64
Edwin Turizo <45569534+proyeckt@users.noreply.github.com>
2020-02-14 22:17:32 -0500

    Archivos Taller 1 Parte 1

---
+++ Talleres/Taller 1 Parte 1/Taller 1 punto 3.R
@@ -0,0 +1,11 @@
+#Metodo Teorema de Taylor
+aproximacion=function (n,x){
+  suma=1
+  i=n-1
+  while(i>0){
+    suma=1+x*suma/i
+    i=i-1
+  }
+  cat(signif(suma,digits=5)) 
+}
+aproximacion(5,0.5)
